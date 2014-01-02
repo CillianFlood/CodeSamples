b696eb17e01ab832f5be2a53fc5dc30885073fc6
CillianFlood <cillianfloood@gmail.com>
2014-01-02 21:42:43 +0000

    Commit

---
+++ CodingSample/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace CodingSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Output = new System.Windows.Forms.Label();
+            this.tallestButton = new System.Windows.Forms.Button();
+            this.shortestButton = new System.Windows.Forms.Button();
+            this.youngestButton = new System.Windows.Forms.Button();
+            this.oldestButton = new System.Windows.Forms.Button();
+            this.meanAgeButton = new System.Windows.Forms.Button();
+            this.healthButton = new System.Windows.Forms.Button();
+            this.stateButton = new System.Windows.Forms.Button();
+            this.weightLossButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(12, 92);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(434, 20000);
+            this.Output.TabIndex = 0;
+            // 
+            // tallestButton
+            // 
+            this.tallestButton.Location = new System.Drawing.Point(12, 12);
+            this.tallestButton.Name = "tallestButton";
+            this.tallestButton.Size = new System.Drawing.Size(75, 23);
+            this.tallestButton.TabIndex = 1;
+            this.tallestButton.Text = "Tallest";
+            this.tallestButton.UseVisualStyleBackColor = true;
+            this.tallestButton.Click += new System.EventHandler(this.tallestButton_Click);
+            // 
+            // shortestButton
+            // 
+            this.shortestButton.Location = new System.Drawing.Point(102, 12);
+            this.shortestButton.Name = "shortestButton";
+            this.shortestButton.Size = new System.Drawing.Size(75, 23);
+            this.shortestButton.TabIndex = 2;
+            this.shortestButton.Text = "Shortest";
+            this.shortestButton.UseVisualStyleBackColor = true;
+            this.shortestButton.Click += new System.EventHandler(this.shortestButton_Click);
+            // 
+            // youngestButton
+            // 
+            this.youngestButton.Location = new System.Drawing.Point(197, 12);
+            this.youngestButton.Name = "youngestButton";
+            this.youngestButton.Size = new System.Drawing.Size(75, 23);
+            this.youngestButton.TabIndex = 3;
+            this.youngestButton.Text = "Youngest";
+            this.youngestButton.UseVisualStyleBackColor = true;
+            this.youngestButton.Click += new System.EventHandler(this.youngestButton_Click);
+            // 
+            // oldestButton
+            // 
+            this.oldestButton.Location = new System.Drawing.Point(291, 12);
+            this.oldestButton.Name = "oldestButton";
+            this.oldestButton.Size = new System.Drawing.Size(75, 23);
+            this.oldestButton.TabIndex = 4;
+            this.oldestButton.Text = "Oldest";
+            this.oldestButton.UseVisualStyleBackColor = true;
+            this.oldestButton.Click += new System.EventHandler(this.oldestButton_Click);
+            // 
+            // meanAgeButton
+            // 
+            this.meanAgeButton.Location = new System.Drawing.Point(15, 53);
+            this.meanAgeButton.Name = "meanAgeButton";
+            this.meanAgeButton.Size = new System.Drawing.Size(75, 23);
+            this.meanAgeButton.TabIndex = 5;
+            this.meanAgeButton.Text = "Mean Age";
+            this.meanAgeButton.UseVisualStyleBackColor = true;
+            this.meanAgeButton.Click += new System.EventHandler(this.meanAgeButton_Click);
+            // 
+            // healthButton
+            // 
+            this.healthButton.Location = new System.Drawing.Point(102, 53);
+            this.healthButton.Name = "healthButton";
+            this.healthButton.Size = new System.Drawing.Size(75, 23);
+            this.healthButton.TabIndex = 6;
+            this.healthButton.Text = "Health";
+            this.healthButton.UseVisualStyleBackColor = true;
+            this.healthButton.Click += new System.EventHandler(this.healthButton_Click);
+            // 
+            // stateButton
+            // 
+            this.stateButton.Location = new System.Drawing.Point(197, 53);
+            this.stateButton.Name = "stateButton";
+            this.stateButton.Size = new System.Drawing.Size(75, 23);
+            this.stateButton.TabIndex = 7;
+            this.stateButton.Text = "By State";
+            this.stateButton.UseVisualStyleBackColor = true;
+            this.stateButton.Click += new System.EventHandler(this.stateButton_Click);
+            // 
+            // weightLossButton
+            // 
+            this.weightLossButton.Location = new System.Drawing.Point(291, 53);
+            this.weightLossButton.Name = "weightLossButton";
+            this.weightLossButton.Size = new System.Drawing.Size(75, 23);
+            this.weightLossButton.TabIndex = 8;
+            this.weightLossButton.Text = "Weight Loss";
+            this.weightLossButton.UseVisualStyleBackColor = true;
+            this.weightLossButton.Click += new System.EventHandler(this.weightLossButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(475, 423);
+            this.Controls.Add(this.weightLossButton);
+            this.Controls.Add(this.stateButton);
+            this.Controls.Add(this.healthButton);
+            this.Controls.Add(this.meanAgeButton);
+            this.Controls.Add(this.oldestButton);
+            this.Controls.Add(this.youngestButton);
+            this.Controls.Add(this.shortestButton);
+            this.Controls.Add(this.tallestButton);
+            this.Controls.Add(this.Output);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Output;
+        private System.Windows.Forms.Button tallestButton;
+        private System.Windows.Forms.Button shortestButton;
+        private System.Windows.Forms.Button youngestButton;
+        private System.Windows.Forms.Button oldestButton;
+        private System.Windows.Forms.Button meanAgeButton;
+        private System.Windows.Forms.Button healthButton;
+        private System.Windows.Forms.Button stateButton;
+        private System.Windows.Forms.Button weightLossButton;
+    }
+}
+
---
+++ CodingSample/Form1.cs
@@ -0,0 +1,439 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CodingSample
+{
+    public partial class Form1 : Form
+    {
+        String file = @"people.csv";
+        const float BMIMax = 30;
+        const float BMIMin = 18.5f;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void tallestButton_Click(object sender, EventArgs e)
+        {
+            string line;
+            string[] words;
+            string output = "";
+            int tallest = 0;
+
+            StreamReader inputStream = File.OpenText(file);
+            inputStream.ReadLine();//Do skip over the Key on the first line of the file.
+            while ((line = inputStream.ReadLine()) != null)//Then in the while loop interate until the end of the file to find the tallest value.
+            {
+                words = line.Split(',');
+
+                if (Convert.ToInt16(words[5]) > tallest)
+                {
+                    tallest = Convert.ToInt16(words[5]);
+                }
+            }
+            inputStream.Close();
+
+            inputStream = File.OpenText(file);//Then we do it again in oreder to output all people with the value found.
+            inputStream.ReadLine();
+            while ((line = inputStream.ReadLine()) != null)
+            {
+                words = line.Split(',');
+
+                if (Convert.ToInt16(words[5]) == tallest)
+                {
+                    output += line + "\n";//Add whoever has the tallest value to the string and place it on a new line.
+                }
+            }
+            inputStream.Close();
+
+            Output.Text = output;//Then out put the combined string of all the people sharing this value.
+        }
+
+        private void shortestButton_Click(object sender, EventArgs e)
+        {
+            string line;
+            string[] words;
+            string output = "";//This entire method works very like the tallest method
+            int shortest = 1000;//Except shortest is initally a large value and we check if values are smaller than it.
+
+            StreamReader inputStream = File.OpenText(file);
+            inputStream.ReadLine();
+            while ((line = inputStream.ReadLine()) != null)
+            {
+                words = line.Split(',');
+
+                if (Convert.ToInt16(words[5]) < shortest)
+                {
+                    shortest = Convert.ToInt16(words[5]);
+                }
+            }
+            inputStream.Close();
+
+            inputStream = File.OpenText(file);
+            inputStream.ReadLine();
+            while ((line = inputStream.ReadLine()) != null)
+            {
+                words = line.Split(',');
+
+                if (Convert.ToInt16(words[5]) == shortest)
+                {
+                    output += line + "\n";
+                }
+            }
+            inputStream.Close();
+
+            Output.Text = output;
+        }
+
+        private void youngestButton_Click(object sender, EventArgs e)
+        {
+            string line;
+            string[] words;
+            string[] date;
+            string output = "";
+            int[] age = {0,0,0};
+
+            StreamReader inputStream = File.OpenText(file);
+            inputStream.ReadLine();
+            while ((line = inputStream.ReadLine()) != null)
+            {
+                words = line.Split(',');//We find the section with the date
+                date = words[2].Split('/');//And split it into the values for days,months,years
+
+                if (Convert.ToInt16(date[2]) > age[2])//We get the largest year
+                {
+                    age[2] = Convert.ToInt16(date[2]);
+                }
+            }
+            inputStream.Close(); 
+            
+            inputStream = File.OpenText(file);
+            inputStream.ReadLine();
+            while ((line = inputStream.ReadLine()) != null)
+            {
+                words = line.Split(',');
+                date = words[2].Split('/');
+
+                if (Convert.ToInt16(date[2]) == age[2] && Convert.ToInt16(date[1]) > age[1])//And once we have the largest year we get the largest month
+                {
+                    age[1] = Convert.ToInt16(date[1]);
+                }
+            }
+            inputStream.Close();
+
+            inputStream = File.OpenText(file);
+            inputStream.ReadLine();
+            while ((line = inputStream.ReadLine()) != null)
+            {
+                words = line.Split(',');
+                date = words[2].Split('/');
+
+                if (Convert.ToInt16(date[2]) == age[2] && Convert.ToInt16(date[1]) == age[1] && Convert.ToInt16(date[0]) > age[0])//Eventually getting the largest day
+                {
+                    age[0] = Convert.ToInt16(date[0]);
+                }
+            }
+            inputStream.Close();
+
+            inputStream = File.OpenText(file);//Now that we have the most recent birth date we check again for the people born on that day
+            inputStream.ReadLine();
+            while ((line = inputStream.ReadLine()) != null)
+            {
+                words = line.Split(',');
+                date = words[2].Split('/');
+
+                if (Convert.ToInt16(date[2]) == age[2] &&
+                    Convert.ToInt16(date[1]) == age[1] &&
+                    Convert.ToInt16(date[0]) == age[0])
+                {
+                    output += line + "\n";
+                }
+            }
+            inputStream.Close();
+
+            Output.Text = output;
+        }
+
+        private void oldestButton_Click(object sender, EventArgs e)
+        {
+            string line;
+            string[] words;
+            string[] date;
+            string output = "";
+            int[] age = { 31, 12, 2014 };
+
+            StreamReader inputStream = File.OpenText(file);
+            inputStream.ReadLine();
+            while ((line = inputStream.ReadLine()) != null)
+            {
+                words = line.Split(',');
+                date = words[2].Split('/');
+
+                if (Convert.ToInt16(date[2]) < age[2])
+                {
+                    age[2] = Convert.ToInt16(date[2]);
+                }
+            }
+            inputStream.Close();
+
+            inputStream = File.OpenText(file);
+            inputStream.ReadLine();
+            while ((line = inputStream.ReadLine()) != null)
+            {
+                words = line.Split(',');
+                date = words[2].Split('/');
+
+                if (Convert.ToInt16(date[2]) == age[2] && Convert.ToInt16(date[1]) < age[1])
+                {
+                    age[1] = Convert.ToInt16(date[1]);
+                }
+            }
+            inputStream.Close();
+
+            inputStream = File.OpenText(file);
+            inputStream.ReadLine();
+            while ((line = inputStream.ReadLine()) != null)
+            {
+                words = line.Split(',');
+                date = words[2].Split('/');
+
+                if (Convert.ToInt16(date[2]) == age[2] && Convert.ToInt16(date[1]) == age[1] && Convert.ToInt16(date[0]) < age[0])
+                {
+                    age[0] = Convert.ToInt16(date[0]);
+                }
+            }
+            inputStream.Close();
+
+            inputStream = File.OpenText(file);
+            inputStream.ReadLine();
+            while ((line = inputStream.ReadLine()) != null)
+            {
+                words = line.Split(',');
+                date = words[2].Split('/');
+
+                if (Convert.ToInt16(date[2]) == age[2] &&
+                    Convert.ToInt16(date[1]) == age[1] &&
+                    Convert.ToInt16(date[0]) == age[0])
+                {
+                    output += line + "\n";
+                }
+            }
+            inputStream.Close();
+
+            Output.Text = output;
+        }
+
+        private void meanAgeButton_Click(object sender, EventArgs e)
+        {
+            string line;
+            string[] words;
+            string[] date;
+            float ageTotal = 0;
+            int counter = 0;
+            int currentYear = 2014;
+
+            StreamReader inputStream = File.OpenText(file);
+            inputStream.ReadLine();
+            while ((line = inputStream.ReadLine()) != null)
+            {
+                words = line.Split(',');
+                date = words[2].Split('/');
+
+                ageTotal += currentYear - Convert.ToInt16(date[2]);//We add the users age in years to the total
+                ageTotal += Convert.ToInt16(date[1]) / 12;         //Divide their month by twelve because its value is twelve times smaller than a year
+                ageTotal += Convert.ToInt16(date[0]) / 365;        //Do the same for the days whoose values are 365 times smaller than that of a year.
+                counter++;                                         //Increase the counter
+            }
+
+            Output.Text = "The Mean age of all applicants is " + ageTotal/counter; //And divide it the total of all the people by the total number to get the mean age.
+
+            inputStream.Close();
+        }
+
+        private void healthButton_Click(object sender, EventArgs e)
+        {
+            string line;
+            string[] words;
+            int overWeight = 0;
+            int underWeight = 0;
+            int healthyWeight = 0;
+
+            StreamReader inputStream = File.OpenText(file);
+            inputStream.ReadLine();
+            while ((line = inputStream.ReadLine()) != null)
+            {
+                words = line.Split(',');
+                float bmi = (Convert.ToInt16(words[6]) / ((Convert.ToInt16(words[5]) / 100) * (Convert.ToInt16(words[5]) / 100)));
+                if (bmi > BMIMax)
+                {
+                    overWeight++;
+                }
+                else if (bmi < BMIMin)
+                {
+                    underWeight++;
+                }
+                else
+                {
+                    healthyWeight++;
+                }
+            }
+
+            Output.Text = "The number of Over Weight people is " + overWeight + "\n" +
+                          "The number of Under Weight people is " + underWeight + "\n"+
+                          "The number of Healthy Weight people is " + healthyWeight;
+
+            inputStream.Close();
+        }
+
+        private void stateButton_Click(object sender, EventArgs e)
+        {
+            string line;
+            string[] words;
+
+            int overWeightState = 0;
+            int underWeightState = 0;
+            int healthyWeightState = 0;
+
+            string outputState1 = "";
+            string outputState2 = "";
+            string outputState3 = "";
+
+            Dictionary<string, int> overWeightDictionary = new Dictionary<string, int>();
+            Dictionary<string, int> underWeightDictionary = new Dictionary<string, int>();
+            Dictionary<string, int> healthyWeightDictionary = new Dictionary<string, int>();
+
+            StreamReader inputStream = File.OpenText(file);
+            inputStream.ReadLine();
+            while ((line = inputStream.ReadLine()) != null)
+            {
+                words = line.Split(',');
+                if (overWeightDictionary.ContainsKey(words[3])){ }//If the string is already in the dictionary we do nothing
+                else
+                {
+                    overWeightDictionary.Add(words[3], 0);//Otherwise we add the state to our dictionary and give it a number of 0
+                }
+
+                if (underWeightDictionary.ContainsKey(words[3])){}//And repeat for each of the three dictionary types
+                else
+                {
+                    underWeightDictionary.Add(words[3], 0);
+                }
+
+                if (healthyWeightDictionary.ContainsKey(words[3])){}
+                else
+                {
+                    healthyWeightDictionary.Add(words[3], 0);
+                }
+            }
+            
+            inputStream = File.OpenText(file);
+            inputStream.ReadLine();
+            while ((line = inputStream.ReadLine()) != null)
+            {
+                words = line.Split(',');
+
+                float bmi = (Convert.ToInt16(words[6]) / ((Convert.ToInt16(words[5]) / 100) * (Convert.ToInt16(words[5]) / 100)));//like before we get the BMI to determine if each person is overweight etc
+                if (bmi > BMIMax)
+                {
+                    overWeightDictionary[words[3]] += 1;//Except this time we add them to the dictionary with the corrisponding state
+                }
+                else if (bmi < BMIMin)
+                {
+                    underWeightDictionary[words[3]] += 1;
+                }
+                else
+                {
+                    healthyWeightDictionary[words[3]] += 1;
+                }
+            }
+
+            foreach (var i in overWeightDictionary.Keys)
+            {
+                if (overWeightDictionary[i] > overWeightState)
+                {
+                    overWeightState = overWeightDictionary[i];//Like with the tallest we then find out which is the heaviest/lightest state.
+                    outputState1 = i;
+                }
+            }
+            foreach (var i in underWeightDictionary.Keys)
+            {
+                if (underWeightDictionary[i] > underWeightState)
+                {
+                    underWeightState = underWeightDictionary[i];//Repeat for each one
+                    outputState2 = i;
+                }
+            }
+            foreach (var i in healthyWeightDictionary.Keys)
+            {
+                if (healthyWeightDictionary[i] > healthyWeightState)
+                {
+                    healthyWeightState = healthyWeightDictionary[i];
+                    outputState3 = i;
+                }
+            }
+
+            //And then output
+            Output.Text = "The state with the most overweight people is " + outputState1 + " with " + overWeightState + " overweight people\n" +
+                          "The state with the most underweight people is " + outputState2 + " with " + underWeightState + " underweight people\n" +
+                          "The state with the most healthyweight people is " + outputState3 + " with " + healthyWeightState + " healthyweight people\n";
+
+            inputStream.Close();
+        }
+
+        private void weightLossButton_Click(object sender, EventArgs e)
+        {
+            string line;
+            string[] words;
+            float totalWeight = 0;
+            float averageWeight = 0;
+            float targetWeight = 0;
+            float targetAverageWeight = 0;
+            int count = 0;
+
+            StreamReader inputStream = File.OpenText(file);
+            inputStream.ReadLine();
+            while ((line = inputStream.ReadLine()) != null)
+            {
+                words = line.Split(',');
+                totalWeight += Convert.ToInt16(words[6]);//We will get the real average weight by adding up all the weights of all the people
+
+                float bmi = (Convert.ToInt16(words[6]) / ((Convert.ToInt16(words[5]) / 100) * (Convert.ToInt16(words[5]) / 100)));
+                if (bmi > BMIMax)
+                {
+                    targetWeight += BMIMax;//But if they are overweight or underweight then we add the max/min weight to the target weight
+                }
+                else if (bmi < BMIMin)
+                {
+                    targetWeight += BMIMin;
+                }
+                else
+                {
+                    targetWeight += Convert.ToInt16(words[6]);
+                }
+                count++;
+            }
+
+            averageWeight = totalWeight / count;//We calculate the average weight of all the people
+            averageWeight = (averageWeight / 100) * 90;//But then we only get 90% of the people because that's all we need
+            targetAverageWeight = targetWeight / count;//We also calcualte the target average weight that everyone should be.
+
+            Output.Text = "The average weight loss needed for 90% of applicants to be healthy is " + (averageWeight - targetAverageWeight) + "Kg";
+            inputStream.Close();
+        }
+    }
+}
